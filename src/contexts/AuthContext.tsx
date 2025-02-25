c4fa5c192dc86cb3c1d21cc5e2452cab2fc498b3
thewoowon <thewoowon@naver.com>
2025-02-25 23:35:10 +0900

    feat: contexts edted and usePermission added

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -11,9 +11,7 @@ export const AuthContext = createContext<AuthContextType | undefined>(
   undefined,
 );
 
-export const AuthProvider: React.FC<{children: React.ReactNode}> = ({
-  children,
-}) => {
+export const AuthProvider = ({children}: {children: React.ReactNode}) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   const initializeAuth = async () => {
--- src/contexts/index.ts
+++ src/contexts/index.ts
@@ -1,2 +1,3 @@
 export {AuthProvider, AuthContext} from './AuthContext';
 // 필요 시 ThemeProvider 등 다른 Context도 여기에 추가
+export {PermissionProvider, usePermission} from './PermissionContext';
